847cff4cf6296fdabaf120c89b3e7d1b43c3ac16
Lyubomir Panov <lyubomir.panov@gmail.com>
2015-08-24 08:49:19 +0300

    Previously implemented a basic Unit of Work Pattern through BlastOFFData.cs file. Added Music Album and Song models.

---
+++ BlastOFF/BlastOFF.Models/MusicModels/Song.cs
@@ -0,0 +1,38 @@
+﻿namespace BlastOFF.Models.MusicModels
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Song
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Artist { get; set; }
+
+        [Required]
+        public string FilePath { get; set; }
+
+        public int? TrackNumber { get; set; }
+
+        public string SongAlbum { get; set; }
+
+        public DateTime? Year { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Comment { get; set; }
+
+        public string AlbumArtist { get; set; }
+
+        public string Composer { get; set; }
+
+        public string Publisher { get; set; }
+
+        public int? Bpm { get; set; }
+    }
+}
