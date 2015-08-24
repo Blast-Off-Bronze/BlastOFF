12156e411cb35e55b4ea8879d8c8607f9516bf5c
Lyubomir Panov <lyubomir.panov@gmail.com>
2015-08-24 10:58:26 +0300

    Created a simple Client project to validate database seeds properly. Successfully seeded the database with a collection of songs.

---
+++ BlastOFF/BlastOFF.Client/BlastOFFClient.cs
@@ -0,0 +1,18 @@
+﻿namespace BlastOFF.Client
+{
+    using System;
+    using System.Linq;
+
+    using Data;
+
+    public class BlastOFFClient
+    {
+        public static void Main()
+        {
+            var dbo = new BlastOFFContext();
+
+            var songsCount = dbo.Songs.Count();
+            Console.WriteLine("Songs count: {0}", songsCount);
+        }
+    }
+}
--- BlastOFF/BlastOFF.Data/Migrations/Configuration.cs
+++ BlastOFF/BlastOFF.Data/Migrations/Configuration.cs
@@ -1,6 +1,10 @@
 namespace BlastOFF.Data.Migrations
 {
     using System.Data.Entity.Migrations;
+    using System.IO;
+    using System.Linq;
+
+    using BlastOFF.Models.MusicModels;
 
     internal sealed class Configuration : DbMigrationsConfiguration<BlastOFFContext>
     {
@@ -11,8 +15,41 @@ namespace BlastOFF.Data.Migrations
             this.ContextKey = "BlastOFF.Data.BlastOFFContext";
         }
 
-        protected override void Seed(BlastOFFContext context)
+        protected override void Seed(BlastOFFContext dbo)
         {
+            //// Add songs
+            if (!dbo.Songs.Any())
+            {
+                string songsInputFile = "../../../database seed files/songs.txt";
+
+                using (var reader = new StreamReader(songsInputFile))
+                {
+                    var line = reader.ReadLine();
+
+                    while (line != null)
+                    {
+                        var songData = line.Split('|');
+
+                        var title = songData[0].Trim();
+                        var artist = songData[1].Trim();
+                        var filePath = songData[2].Trim();
+                        var viewsCount = int.Parse(songData[3].Trim());
+
+                        dbo.Songs.Add(
+                            new Song
+                            {
+                                Title = title,
+                                Artist = artist,
+                                FilePath = filePath,
+                                ViewsCount = viewsCount
+                            });
+
+                        line = reader.ReadLine();
+                    }
+                }
+
+                dbo.SaveChanges();
+            }
         }
     }
 }
