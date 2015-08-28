8ea8fe9cdda21c491a461ee5bd93d230f3c578e3
tushe <tushe2000@gmail.com>
2015-08-28 11:35:06 +0300

    removed req fields from chatBind

--- BlastOFF/BlastOFF.Services/Models/ChatModels/ChatBindingModel.cs
+++ BlastOFF/BlastOFF.Services/Models/ChatModels/ChatBindingModel.cs
@@ -1,16 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace BlastOFF.Services.Models.ChatModels
+﻿namespace BlastOFF.Services.Models.ChatModels
 {
     public class ChatBindingModel
     {
-        [Required]
         public string Content { get; set; }
 
-        [Required]
         public string ChatRoomName { get; set; }
 
-        [Required]
         public string ReceiverUsername { get; set; }
     }
 }
