eaf843db0b9079850b1d9f611d030f8bba139017
enevlogiev <edward.evlogiev@gmail.com>
2015-08-28 22:02:43 +0300

    added user follow/unfollow endpoints

--- BlastOFF.BackEnd/BlastOFF.Services/Controllers/UsersController.cs
+++ BlastOFF.BackEnd/BlastOFF.Services/Controllers/UsersController.cs
@@ -64,5 +64,67 @@
             return this.Ok(userProfile);
 
         }
+
+        [HttpPost]
+        [Route("api/users/{username}/follow")]
+        [Authorize]
+        public IHttpActionResult FollowUser(string username)
+        {
+            var loggedUserId = this.User.Identity.GetUserId();
+            var searchedUser = this.Data.Users.All().FirstOrDefault(u => u.UserName == username);
+
+            if (searchedUser == null)
+            {
+                return this.NotFound();
+            }
+
+            if (loggedUserId == searchedUser.Id)
+            {
+                //cant follow yourself
+                return this.BadRequest();
+            }
+
+            if (searchedUser.FollowedBy.Any(f => f.Id == loggedUserId))
+            {
+                //already follow by you
+                return this.BadRequest();
+            }
+
+            var currentUser = this.Data.Users.All().First(u => u.Id == loggedUserId);
+
+            currentUser.FollowedUsers.Add(searchedUser);
+            searchedUser.FollowedBy.Add(currentUser);
+
+            this.Data.SaveChanges();
+            return this.Ok();
+        }
+
+        [HttpDelete]
+        [Authorize]
+        [Route("api/users/{username}/unfollow")]
+        public IHttpActionResult UnfollowUser(string username)
+        {
+            var loggedUserId = this.User.Identity.GetUserId();
+            var searchedUser = this.Data.Users.All().FirstOrDefault(u => u.UserName == username);
+
+            if (searchedUser == null)
+            {
+                return this.NotFound();
+            }
+
+            if (!searchedUser.FollowedBy.Any(f => f.Id == loggedUserId))
+            {
+                //you are not following this user anyway
+                return this.BadRequest();
+            }
+
+            var currentUser = this.Data.Users.All().First(u => u.Id == loggedUserId);
+
+            currentUser.FollowedUsers.Remove(searchedUser);
+            searchedUser.FollowedBy.Remove(currentUser);
+
+            this.Data.SaveChanges();
+            return this.Ok();
+        }
     }
 }
