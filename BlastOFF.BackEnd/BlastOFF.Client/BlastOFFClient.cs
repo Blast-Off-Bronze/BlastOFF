b44134fe0b4ca33558b21ada265974455982913e
Lyubomir-Panov <lyubomir.panov@gmail.com>
2015-09-11 01:32:10 +0300

    Fixed an issue causing the program to crash when uploading song to Google Drive from deployed Web API project

--- BlastOFF.BackEnd/BlastOFF.Client/BlastOFFClient.cs
+++ BlastOFF.BackEnd/BlastOFF.Client/BlastOFFClient.cs
@@ -1,26 +1,9 @@
 ﻿namespace BlastOFF.Client
 {
-    using System;
-    using System.Linq;
-
-    using BlastOFF.Data;
-
     public class BlastOFFClient
     {
         public static void Main()
         {
-            var dbo = new BlastOFFContext();
-
-            var songsCount = dbo.Songs.Count();
-
-            Console.WriteLine("Total songs: " + songsCount);
-
-            var albums = dbo.MusicAlbums.ToList();
-
-            foreach (var musicAlbum in albums)
-            {
-                Console.WriteLine(musicAlbum.Followers.Count);
-            }
         }
     }
 }
